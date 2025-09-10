73aeac92a532ddbc9bdf9a93a7b74d5d519cc0a1
Matheus Santos <Matheus.soliveira11@hotmail.com>
2025-09-10 09:16:46 -0300

    sla

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,27 @@
+export default function Home() {
+    return (
+    <div className="min-h-screen flex flex-col bg-[#0c0c0c] text-white">
+        <header className="w-full shadow-md z-2">
+            <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-4">
+ 
+                <div className="w-12" />
+ 
+                <div className="flex-grow max-w-md mx-6">
+                    <input
+                    type="text"
+                    placeholder="Pesquisar..."
+                    className="w-full px-4 py-2 rounded-lg bg-white text-black placeholder-gray-400"
+                    />
+                </div>
+ 
+            </div>
+            <div className="w-full h-0.5 bg-[#ec4d9d]"></div>
+        </header>
+ 
+ 
+        {/*Conteúdo das página */}
+
+    </div>
+    );
+}
+ 
